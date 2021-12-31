89e078672bea9d5996c71be75d8ccbeec161a346
Gavin Osborn <75707967+gavosb@users.noreply.github.com>
2021-12-31 15:51:55 -0500

    Update ServerBrowser.js

--- src/components/ServerBrowser.js
+++ src/components/ServerBrowser.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Home from './Home';
 import '../App.css';
 
-
-
+/*
+    The ServerBrowser component, where the server list is displayed alongside buttons to join rooms.
+    This component takes Home's state function as a prop, so that it can change the page to a new room.
+*/
 const ServerBrowser = ({setParentComponent}) => {
     
     let codes = [];
-
+    
+    /*
+        Builds an array from the array of peers from the peer server's API. Called every time the page is refreshed.
+        I'm going to be honest, I was incredibly lazy here - I had been working on this for a week and just didn't want to bother.
+        This should implement useState like in Home.js. I'll probably fix it later. Complicated due to async.
+    */
     async function getPeers (){
         let obj;
         const response = await fetch("https://"+location.hostname + ":9000/pecker/peers");
