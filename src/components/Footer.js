1d6478a5c3f35b7e9fd9a94793fcb2c99b33ab7f
Gavin Osborn <75707967+gavosb@users.noreply.github.com>
2021-12-31 14:49:16 -0500

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+const Footer = () => {
+    return (
+       <div className='Footer'>
+            <footer>
+            <p><a href="https://gavosb.com">2021 Gavin Osborn</a></p>
+            </footer> 
+        </div>
+    );
+}
+ 
+export default Footer;
